b8ed9dab218aa3201150ea690ce0a618767f802a
charliekappa <sharlyebonham@gmail.com>
2016-10-01 05:08:03 -0500

    API WORKING

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-  $.getJSON('')
+  $.getJSON('https://api.twitch.tv/kraken/channels/charliekappa?client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json) {
+    console.log(json);
+
+  });
 
 });
