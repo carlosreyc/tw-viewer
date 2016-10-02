36baab8f828773c90b803aa7bdb4f203bb4219be
charliekappa <sharlyebonham@gmail.com>
2016-10-02 00:15:45 -0500

    Getting better at this

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
   $.getJSON('https://api.twitch.tv/kraken/channels/charliekappa?client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json) {
     console.log(json);
+    console.log(json.display_name);
+    $("div.name1").html(json.display_name);
+    $("a.link1").attr('href', json.url);
+    $("div.status1").html(json.status);
+  });
+  $.getJSON('https://api.twitch.tv/kraken/streams?channel=riotgames&client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json){
+    console.log(json);
+
+  });
+  $.getJSON('https://api.twitch.tv/kraken/streams?channel=charliekappa&client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json){
+    console.log(json);
 
   });
 
