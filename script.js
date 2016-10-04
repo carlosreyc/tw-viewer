7b6c8808e94560afac1e5dcf9e00da8a10255d84
charliekappa <sharlyebonham@gmail.com>
2016-10-04 17:36:01 -0500

    Backup

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $("a.link1").attr('href', json.url);
     $("div.status1").html(json.status);
   });
-  $.getJSON('https://api.twitch.tv/kraken/streams?channel=riotgames&client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json){
+  $.getJSON('https://api.twitch.tv/kraken/streams?channel=c9sneaky,charliekappa,lahuellalol&client_id=fkyh8yy1cvieny2uhhd2msmizfc5wed&callback=?', function(json){
     console.log(json);
 
   });
